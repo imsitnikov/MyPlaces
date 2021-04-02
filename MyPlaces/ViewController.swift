705c2009358c84be04afbf93bb27827f9e73dff4
Sitnikov Maksim <Maksim.SITNIKOV@raiffeisen.ru>
2021-04-02 11:26:49 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by SITNIKOV Maksim on 02.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
