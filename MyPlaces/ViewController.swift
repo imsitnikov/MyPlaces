056f667913d154c09e5b71e196af666fb4001c4c
Sitnikov Maksim <Maksim.SITNIKOV@raiffeisen.ru>
2021-04-03 17:57:50 +0300

    Initial

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell \(indexPath.row)"
+        return cell!
+    }
 }
 
